e68a5154be7e593f7964836f4a4494059d8b6693
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-25 13:18:59 -0500

    Adjust spacing to make flow through statment more clear

--- db/query.sql
+++ db/query.sql
@@ -6,6 +6,7 @@ ON department.id = role.department_id;
 
 -- Joins to provide employee table
 SELECT e.id, e.first_name, e.last_name, role.title, department.name as department, role.salary, CONCAT(m.first_name, ' ', m.last_name) as manager
-FROM employee AS e LEFT OUTER JOIN employee AS m ON e.manager_id = m.id
+FROM employee AS e 
+LEFT OUTER JOIN employee AS m ON e.manager_id = m.id
 JOIN role ON role.id = e.role_id
 INNER JOIN department ON department.id = role.department_id;
