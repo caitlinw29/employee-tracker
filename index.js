f79b4b68aaec25fa610b63f4fccc7525717c18be
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-25 13:26:31 -0500

    Add queries for role table and employee table

--- index.js
+++ index.js
@@ -9,11 +9,21 @@ const db = mysql.createConnection(
     user: 'root',
     password: '',
     database: 'employees_db'
-  },
-  console.log(`Connected to the employees_db database.`)
+  }
 );
 
-// Query database
+// Query database for department table
 db.query('SELECT * FROM department', function (err, results) {
+  console.table(results);
+});
+
+// Query database for role table
+db.query('SELECT role.id, role.title, department.name as department, role.salary FROM role JOIN department ON department.id = role.department_id', function (err, results) {
+  console.table(results);
+});
+
+
+//Query database for employee table
+db.query('SELECT e.id, e.first_name, e.last_name, role.title, department.name as department, role.salary, CONCAT(m.first_name, " ", m.last_name) as manager FROM employee AS e  LEFT OUTER JOIN employee AS m ON e.manager_id = m.id JOIN role ON role.id = e.role_id INNER JOIN department ON department.id = role.department_id', function (err, results) {
   console.table(results);
 });
