18a8dd46ff596e92209e393dfaa7e12ed75c6050
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-25 12:40:58 -0500

    Adjust to use manager names in employee table

--- db/query.sql
+++ db/query.sql
@@ -5,8 +5,7 @@ JOIN department
 ON department.id = role.department_id;
 
 -- Joins to provide employee table
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(employee.first_name, ' ', employee.last_name) as manager
-FROM employee
-JOIN role
-ON role.id = employee.role_id
+SELECT e.id, e.first_name, e.last_name, role.title, department.name as department, role.salary, CONCAT(m.first_name, ' ', m.last_name) as manager
+FROM employee AS e LEFT OUTER JOIN employee AS m ON e.manager_id = m.id
+JOIN role ON role.id = e.role_id
 INNER JOIN department ON department.id = role.department_id;
