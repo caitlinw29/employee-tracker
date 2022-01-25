47dec45a555f61fa0f8070662e0a4a9d407050b4
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-25 11:53:51 -0500

    Pull together first and last name to make a full name, not tied to manager yet

--- db/query.sql
+++ db/query.sql
@@ -5,7 +5,7 @@ JOIN department
 ON department.id = role.department_id;
 
 -- Joins to provide employee table
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, employee.id as manager
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(employee.first_name, ' ', employee.last_name) as manager
 FROM employee
 JOIN role
 ON role.id = employee.role_id
