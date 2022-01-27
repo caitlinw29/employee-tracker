5ffe03f2f5e99321feb3d9f060498d3989e15c64
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-26 19:23:58 -0500

    Sort by id

--- helpers/queries.js
+++ helpers/queries.js
@@ -27,8 +27,8 @@ class Query {
 
 //create instances of the query class for each table
 const department = new Query('SELECT * FROM department');
-const role = new Query('SELECT role.id, role.title, department.name as department, role.salary FROM role JOIN department ON department.id = role.department_id');
-const employee = new Query('SELECT e.id, e.first_name, e.last_name, role.title, department.name as department, role.salary, CONCAT(m.first_name, " ", m.last_name) as manager FROM employee AS e  LEFT OUTER JOIN employee AS m ON e.manager_id = m.id JOIN role ON role.id = e.role_id INNER JOIN department ON department.id = role.department_id');
+const role = new Query('SELECT role.id, role.title, department.name as department, role.salary FROM role JOIN department ON department.id = role.department_id ORDER BY id');
+const employee = new Query('SELECT e.id, e.first_name, e.last_name, role.title, department.name as department, role.salary, CONCAT(m.first_name, " ", m.last_name) as manager FROM employee AS e  LEFT OUTER JOIN employee AS m ON e.manager_id = m.id JOIN role ON role.id = e.role_id INNER JOIN department ON department.id = role.department_id ORDER BY id');
 
 
 //put all of the current roles in an array
